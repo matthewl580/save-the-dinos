35eb7195ac9850a4fe744f5fc31edd0bafe36b70
Matthew Lozier-Davis <matthewlozier580@gmail.com>
2025-04-12 04:38:45 +0000

    It should have a navbar with a set of pages. this is a charity website

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-background font-pixelated">
+      <h1 className="text-4xl mb-4">About Us</h1>
+      <p className="text-lg text-center">
+        This is the about page for our charity website.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-primary text-primary-foreground p-4 font-pixelated">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="text-xl font-bold">Charity Site</Link>
+        <div className="space-x-4">
+          <Link href="/" className="hover:underline">Home</Link>
+          <Link href="/about" className="hover:underline">About</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
